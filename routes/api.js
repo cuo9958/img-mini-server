b632201bef7b7859005168f1c131a5e3ff85ea57
guofangchao <guofangchao@daling.com>
2019-07-01 18:51:18 +0800

    准确识别图片格式

--- routes/api.js
+++ routes/api.js
@@ -27,21 +27,36 @@ const upload2 = multer({
 
 router.post('/upload/img', upload2.single('file'), async function (req, res, next) {
     let file_buffer = null;
+    const headType = req.file.buffer.toString("hex", 0, 8)
+    const headType2 = req.file.buffer.toString("hex", req.file.buffer.length - 2)
+    let imgType = '';
+    if (headType.indexOf('89504e470d0a1a0a') === 0) {
+        imgType = 'png'
+    }
+    if (headType.indexOf('474946383961') === 0) {
+        imgType = 'gif'
+    }
+    if (headType.indexOf('474946383761') === 0) {
+        imgType = 'gif'
+    }
+    if (headType.indexOf('ffd8') === 0 && headType2 === 'ffd9') {
+        imgType = 'jpg'
+    }
     if (req.query.mini === 0) {
         file_buffer = req.file.buffer;
     } else {
-        if (/\.(jpg|jpeg)$/i.test(req.file.originalname)) {
+        if (imgType === 'jpg') {
             file_buffer = await Jpg(req.file.buffer);
         }
-        if (/\.png$/i.test(req.file.originalname)) {
+        if (imgType === 'png') {
             file_buffer = await Png(req.file.buffer);
         }
-        if (/\.gif$/i.test(req.file.originalname)) {
+        if (imgType === 'gif') {
             file_buffer = await Gif(req.file.buffer);
         }
     }
 
-    if (file_buffer === null) {
+    if (file_buffer === null || imgType === '') {
         return res.json({
             code: 0,
             msg: "图片格式不支持"
@@ -51,7 +66,7 @@ router.post('/upload/img', upload2.single('file'), async function (req, res, nex
     let old_name = req.file.originalname;
     console.log("上传文件", old_name);
     let ext = old_name.split(".");
-    let filename = "img_" + Date.now() + Math.round(Math.random() * 100) + "." + ext[ext.length - 1];
+    let filename = "img_" + Date.now() + Math.round(Math.random() * 100) + "." + imgType;
 
     let file_size = 0;
     file_size = file_buffer.length;
